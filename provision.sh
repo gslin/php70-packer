8c24feed163ef1b8cc544090587dbd6d23d4c4be
Gea-Suan Lin <gslin@gslin.org>
2017-01-16 02:18:43 +0800

    Import provision.sh.

---
+++ provision.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-add-repository -y ppa:ondrej/php
+
+sudo apt-get -y update
+sudo apt-get -y dist-upgrade
+
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install apache2-bin
+sudo a2dismod mpm_event
+sudo a2enmod mpm_worker
+sudo service apache2 restart
+
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install php7.0-{bcmath,cgi,cli,common,curl,fpm,gd,gmp,json,mbstring,mcrypt,mysql,opcache,readline,sqlite3,xml,xmlrpc,xsl,zip}
+
+sudo apt-get -y clean
