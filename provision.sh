c4f5d51b2e011543b998e2af8a728406e7183f50
Gea-Suan Lin <gslin@gslin.org>
2017-01-16 03:49:32 +0800

    Install codedeploy.

--- provision.sh
+++ provision.sh
@@ -12,4 +12,10 @@ sudo service apache2 restart
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install php7.0-{bcmath,cgi,cli,common,curl,fpm,gd,gmp,json,mbstring,mcrypt,mysql,opcache,readline,sqlite3,xml,xmlrpc,xsl,zip}
 
+# AWS CodeDeploy
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install ruby
+cd /tmp; wget https://aws-codedeploy-ap-northeast-1.s3.amazonaws.com/latest/install
+chmod 755 install
+sudo ./install auto
+
 sudo apt-get -y clean
